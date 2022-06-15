7d9e503807b52a2f95ec6357c4c346256a87a7a3
Kholison Ansharulloh Azizi <azizikholis16@gmail.com>
2022-06-15 14:30:46 +0700

    praktikum 9

---
+++ praktikum9/src/main/java/com/kholison_19102147/praktikum9/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.kholison_19102147.praktikum9
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.kholison_19102147.praktikum9.data.SettingModel
+import com.kholison_19102147.praktikum9.databinding.ActivityMainBinding
+import com.kholison_19102147.praktikum9.preference.SettingPreference
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: com.kholison_19102147.praktikum9.databinding.ActivityMainBinding
+    private lateinit var settingModel: SettingModel
+    private lateinit var mSettingPreference: SettingPreference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.act = this
+        setContentView(binding.root)
+        supportActionBar?.title = getString(R.string.main_title)
+        mSettingPreference = SettingPreference(this)
+        showExistingPreference()
+    }
+    private fun showExistingPreference() {
+        settingModel = mSettingPreference.getSetting()
+        populateView(settingModel)
+    }
+    private fun populateView(settingModel: SettingModel) {
+        if (settingModel.isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+        }
+        binding.settingModel = settingModel
+    }
+    fun openSetting(){
+        val intent = Intent(this@MainActivity, SettingPreferenceActivity::class.java)
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+    companion object {
+        private const val REQUEST_CODE = 100
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == SettingPreferenceActivity.RESULT_CODE) {
+                showExistingPreference()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                openSetting()
+            }
+        }
+    }
+
+}
