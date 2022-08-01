389791907c9c154672100664f3df015c7905cd83
Kholison Ansharulloh Azizi <azizikholis16@gmail.com>
2022-08-01 17:36:47 +0700

    praktikum 14

---
+++ praktikum12/src/main/java/com/fahririzmawan_19102138/praktikum12/MainActivity.kt
@@ -0,0 +1,132 @@
+package com.fahririzmawan_19102138.praktikum12
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.fahririzmawan_19102138.praktikum12.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(this@MainActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.btnSignOut.setOnClickListener(this)
+        binding.btnEmailVerify.setOnClickListener(this)
+        binding.btnQuote.setOnClickListener{
+            val btnIntent = Intent(this, DashboardQuoteActivity::class.java)
+            startActivity(btnIntent)
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+    private fun updateUI(currentUser: FirebaseUser) {
+        binding.btnEmailVerify.isVisible = false
+        currentUser?.let {
+            val name = currentUser.displayName
+            val phoneNumber = currentUser.phoneNumber
+            val email = currentUser.email
+            val photoUrl = currentUser.photoUrl.toString()
+            val emailVerified = currentUser.isEmailVerified
+            val uid = currentUser.uid
+            binding.tvName.text = name
+            if(TextUtils.isEmpty(name)){
+                binding.tvName.text = "No Name"
+            }
+            binding.tvUserId.text = email
+            for (profile in it.providerData) {
+                val providerId = profile.providerId
+                if(providerId=="password" && emailVerified==false){
+                    binding.btnEmailVerify.isVisible = true
+                }
+                if(providerId=="phone"){
+                    binding.tvName.text = phoneNumber
+                    binding.tvUserId.text = providerId
+                }
+            }
+            if (photoUrl !== null){
+                Glide.with(this)
+                    .load(photoUrl)
+                    .apply(RequestOptions().override(700, 700))
+                    .into(binding.ivImage)
+            }
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(this@MainActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            updateUI(currentUser)
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnSignOut -> {
+                signOut()
+            }
+            R.id.btnEmailVerify -> {
+                sendEmailVerification()
+            }
+        }
+    }
+    private fun sendEmailVerification() {
+        binding.btnEmailVerify.isEnabled = false
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                binding.btnEmailVerify.isEnabled = true
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext,
+                        "Verification email sent to ${user.email} ",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(baseContext,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    private fun signOut() {
+        auth.signOut()
+        val currentUser = auth.currentUser
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+        }
+        if (currentUser == null) {
+            val intent = Intent(this@MainActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
